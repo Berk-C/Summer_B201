195aff698677ad2f0bb699afb247951cfe239f99
Berk-C <cankaya439@gmail.com>
2020-08-25 18:24:51 -0500

    Initial commit

---
+++ src/day0/MethodCall.java
@@ -0,0 +1,11 @@
+package day0;
+
+public class MethodCall {
+    public static void main(String[] args) {
+        String str = "Cybertek School";
+
+
+        String str2 = "abcabcaaaaabbbbbbcccccddddeeeeeee";
+    }
+
+}
---
+++ src/day02_printStatements/HelloWorld.java
@@ -0,0 +1,16 @@
+package day02_printStatements;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println( "Hello Wolrd" );
+
+    }
+
+}
+
+                        /*
+                        Today we learned HelloWorld
+                           struggles are: print statement
+                                          main method
+                                          date:
+                         */
